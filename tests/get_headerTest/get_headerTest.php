222fbe2819cd5b15e9b22cbf5a0b04b4e46acb04
Yakeing <yakeing@gmail.com>
2017-08-13 22:29:45 +0800

    Delete get_headerTest.php

--- tests/get_headerTest/get_headerTest.php
+++
@@ -1,9 +0,0 @@
-
-class get_headerTest extends PHPUnit_Framework_TestCase{
-  
-  public function testGet(){
-    $url = "https://github.com/yakeing/get_header";
-    $GetHeader = get_header::curlHeader($url);
-    $this->assertEquals(200, $GetHeader['code']);
-  }
-}
