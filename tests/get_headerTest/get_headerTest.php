2eda06c8b72eb00ee5611051bc74baf7f37b7458
Yakeing <yakeing@gmail.com>
2017-08-13 20:47:44 +0800

    Update get_headerTest.php

--- tests/get_headerTest/get_headerTest.php
+++ tests/get_headerTest/get_headerTest.php
@@ -2,11 +2,11 @@
 class get_headerTest extends PHPUnit_Framework_TestCase{
 
   public function testGet(){
+    $this->assertTrue(class_exists('get_header'));
     $url = "https://github.com/yakeing/get_header";
     $result = get_header::curlHeader($url);
     $this->assertInternalType("array", $result);
-    //$this->assertEquals(200, $result['code']);
-    $this->assertTrue(200, $result['code']);
+    $this->assertEquals(200, $result['code']);
   }
 
 }
