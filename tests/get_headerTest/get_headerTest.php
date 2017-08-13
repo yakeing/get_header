08427c7fe04e7c22f68dee73b63bf2e761159e8a
yakeing <yakeing@gmail.com>
2017-08-14 02:02:08 +0800

    b2

--- tests/get_headerTest/get_headerTest.php
+++ tests/get_headerTest/get_headerTest.php
@@ -3,8 +3,7 @@ class get_headerTest extends PHPUnit_Framework_TestCase{
 
   public function testGet(){
     $url = "https://github.com/yakeing/get_header";
-    $GetHeader = new get_header();
-    $Header = $GetHeader->curlHeader($url);
+    $Header = get_header::curlHeader($url);
     $this->assertTrue(is_array($Header));
     $this->assertEquals(200, $Header['code']);
   }
