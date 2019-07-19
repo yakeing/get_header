3f421cfd193907bbb15617319bb1e1cdc7035330
Yakeing <yakeing@gmail.com>
2019-07-19 16:20:50 +0800

    bootstrap.php Restoration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,4 @@
 <?php
-/*
-if (file_exists('vendor/autoload.php')) {
-    require_once 'vendor/autoload.php';
-} else {
-    require_once '../vendor/autoload.php';
-}
-*/
+
 $loader = include __DIR__ . '/../vendor/autoload.php';
 $loader->add('get_header', __DIR__ . '/../src');
