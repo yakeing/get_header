717fa066164f2eae00dfb7094cd264223f3b4c45
Yakeing <yakeing@gmail.com>
2019-07-19 17:06:21 +0800

    $loader = include __DIR__ . '/../vendor/autoload.php'; $loader->add('php_fsockopen', __DIR__ . '/../src');

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,3 @@
 <?php
-$loader = include __DIR__ '/../vendor/autoload.php';
-//$loader->add('get_header', __DIR__ '/../src');
+$loader = include __DIR__ . '/../vendor/autoload.php';
+$loader->add('php_fsockopen', __DIR__ . '/../src');
