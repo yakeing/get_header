5cf83a05fcd74de89eaed85e6d6f56b12da312c3
Yakeing <yakeing@gmail.com>
2019-07-19 17:00:05 +0800

    Restoration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,3 @@
 <?php
-if (file_exists( __DIR__ . '/vendor/autoload.php')) {
-    $dir =  __DIR__ ;
-} else {
-    $dir =  __DIR__ '/..';
-}
-$loader = include $dir. '/vendor/autoload.php';
-$loader->add('get_header', $dir . '/src');
+$loader = require_once __DIR__ '/../vendor/autoload.php';
+$loader->add('get_header', __DIR__ '/../src');
