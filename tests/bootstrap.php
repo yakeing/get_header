a3804d70511a3ec38c8d7d2a674735de52d40399
Yakeing <yakeing@gmail.com>
2019-07-19 15:59:47 +0800

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
-$loader = include __DIR__ . '/vendor/autoload.php';
+if (file_exists('vendor/autoload.php')) {
+    require_once 'vendor/autoload.php';
+} else {
+    require_once '../vendor/autoload.php';
+}
+//$loader = include __DIR__ . '/../vendor/autoload.php';
 //$loader->add('get_header', __DIR__ . '/../src');
